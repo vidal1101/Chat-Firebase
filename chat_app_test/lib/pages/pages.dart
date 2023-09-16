04df4e2b46391a656d0d6e0264a4302a24afe0db
vidal1101 <rodrigovidal278@gmail.com>
2023-09-15 21:17:42 -0600

    add: pantalla para verificar el login o home a traves del providers

--- chat_app_test/lib/pages/pages.dart
+++ chat_app_test/lib/pages/pages.dart
@@ -1,7 +1,8 @@
 
 
+export 'package:chat_app_test/pages/login_page.dart';
+
+export 'package:chat_app_test/pages/SplashPage.dart';
+
 export 'package:chat_app_test/pages/home_page.dart';
-
-///archivo para exportaciones de rutas 
-
 
